782b6c122fa9316607091d82a10c4611ffcd7edf
Sheron <sherony060117@gmail.com>
2025-07-15 00:01:43 -0400

    update

--- script.js
+++ script.js
@@ -1,355 +1,185 @@
-import GUI from "https://cdn.jsdelivr.net/npm/lil-gui@0.18.2/+esm"
+class Scene {
+    constructor(options) {
+        this.$el = options.el;
+        this.time = 0;
+        this.mouse = {
+            x: 0,
+            y: 0
+        };
+        
+        this.spheres = [];
+        this.lightSystems = [];
 
-const canvasEl = document.querySelector("canvas");
-const textureEl = document.createElement("canvas");
-const textureCtx = textureEl.getContext("2d");
-
-const fontOptions = {
-    "Arial": "Arial, sans-serif",
-    "Verdana": "Verdana, sans-serif",
-    "Tahoma": "Tahoma, sans-serif",
-    "Times New Roman": "Times New Roman, serif",
-    "Georgia": "Georgia, serif",
-    "Garamond": "Garamond, serif",
-    "Courier New": "Courier New, monospace",
-    "Brush Script MT": "Brush Script MT, cursive"
-}
-
-const params = {
-    fontName: "Verdana",
-    isBold: false,
-    fontSize: 80,
-    text: "fluid",
-    pointerSize: null,
-    color: {r: 1., g: .0, b: .5}
-};
-
-const pointer = {
-    x: 0,
-    y: 0,
-    dx: 0,
-    dy: 0,
-    moved: false,
-};
-
-
-let outputColor, velocity, divergence, pressure, canvasTexture;
-let isPreview = true;
-
-const gl = canvasEl.getContext("webgl");
-gl.getExtension("OES_texture_float");
-
-const vertexShader = createShader(
-    document.getElementById("vertShader").innerHTML,
-    gl.VERTEX_SHADER);
-
-const splatProgram = createProgram("fragShaderPoint");
-const divergenceProgram = createProgram("fragShaderDivergence");
-const pressureProgram = createProgram("fragShaderPressure");
-const gradientSubtractProgram = createProgram("fragShaderGradientSubtract");
-const advectionProgram = createProgram("fragShaderAdvection");
-const outputShaderProgram = createProgram("fragShaderOutputShader");
-
-gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-    -1, -1,
-    -1, 1,
-    1, 1,
-    1, -1
-]), gl.STATIC_DRAW);
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gl.createBuffer());
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 0, 2, 3]), gl.STATIC_DRAW);
-gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(0);
-
-
-createTextCanvasTexture();
-initFBOs();
-createControls();
-setupEvents();
-resizeCanvas();
-window.addEventListener("resize", resizeCanvas);
-
-render();
-
-function createTextCanvasTexture() {
-    canvasTexture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, canvasTexture);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-}
-
-function updateTextCanvas() {
-    textureCtx.fillStyle = "black";
-    textureCtx.fillRect(0, 0, textureEl.width, textureEl.height);
-
-    textureCtx.font = (params.isBold ? "bold" : "normal") + " " + (params.fontSize * devicePixelRatio) + "px " + fontOptions[params.fontName];
-    textureCtx.fillStyle = "#ffffff";
-    textureCtx.textAlign = "center";
-
-    textureCtx.filter = "blur(3px)";
-
-    const textBox = textureCtx.measureText(params.text);
-    textureCtx.fillText(params.text, .5 * textureEl.width, .5 * textureEl.height + .5 * textBox.actualBoundingBoxAscent);
-
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, canvasTexture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textureEl);
-}
-
-function createProgram(elId) {
-    const shader = createShader(
-        document.getElementById(elId).innerHTML,
-        gl.FRAGMENT_SHADER);
-    const program = createShaderProgram(vertexShader, shader);
-    const uniforms = getUniforms(program);
-    return {
-        program, uniforms
+        this.bindAll();
+        this.init();
     }
-}
-
-function createShaderProgram(vertexShader, fragmentShader) {
-    const program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        console.error("Unable to initialize the shader program: " + gl.getProgramInfoLog(program));
-        return null;
+    
+    bindAll() {
+        this.render = this.render.bind(this);
+        this.mousemove = this.mousemove.bind(this);
+        this.resize = this.resize.bind(this);
     }
+    
+    init() {
+        this.textureLoader = new THREE.TextureLoader();
+                                                // 40
+        this.camera = new THREE.PerspectiveCamera( 15, window.innerWidth / window.innerHeight, 1, 2000 );
+		this.camera.position.z = 10; //450
+        this.camera.position.y = 800;
+        this.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-    return program;
-}
+		this.scene = new THREE.Scene();
 
-function getUniforms(program) {
-    let uniforms = [];
-    let uniformCount = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
-    for (let i = 0; i < uniformCount; i++) {
-        let uniformName = gl.getActiveUniform(program, i).name;
-        uniforms[uniformName] = gl.getUniformLocation(program, uniformName);
+		this.renderer = new THREE.WebGLRenderer({ alpha: true });
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+	    this.renderer.setSize( window.innerWidth, window.innerHeight );
+        //this.renderer.setClearColor(0xffffff, 1.0);
+        this.$el.appendChild( this.renderer.domElement );
+        
+        // this.createSpheres();
+        this.createLights();
+        this.createParticles();
+        
+        this.bindEvents();
+        this.resize();
+        this.render();
     }
-    return uniforms;
-}
+    
+    createParticles() {
+        // Geometry that is used to spawn the particles
+        // const geometry = new THREE.Geometry();
+        // for ( let i = 0; i < 10000; i ++ ) {
+        //     let vertex = new THREE.Vector3();
+        //     vertex.x = Math.random() * 60 - 30;
+        //     vertex.y = Math.random() * 20 - 10;
+        //     vertex.z = Math.random() * 0 - 10;
+        //     geometry.vertices.push( vertex );
+        // }
+        
+        const plane = new THREE.PlaneBufferGeometry(500, 250, 250, 125);
+        
+        const textureLoader = new THREE.TextureLoader();
+        textureLoader.crossOrigin = '';
+        
+        const material = new THREE.ShaderMaterial( {
+            uniforms: {
+                time: { value: 1.0 },
+                texture:   { value: textureLoader.load( "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1081752/spark1.png" ) },
+                resolution: { value: new THREE.Vector2() }
 
-function createShader(sourceCode, type) {
-    const shader = gl.createShader(type);
-    gl.shaderSource(shader, sourceCode);
-    gl.compileShader(shader);
+            },
 
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        console.error("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
-        gl.deleteShader(shader);
-        return null;
+            vertexShader: document.getElementById( 'render-vs' ).textContent,
+            fragmentShader: document.getElementById( 'render-fs' ).textContent,
+            // blending: THREE.AdditiveBlending,
+            depthTest: false,
+            transparent: true
+
+        } );
+        
+        console.log(material.uniforms.texture);
+		
+        //const material = new THREE.PointsMaterial( { size: 1 } );
+        this.particles = new THREE.Points( plane, material );
+        this.particles.rotation.x = this.degToRad(-90);
+
+        this.scene.add(this.particles);
     }
+    
+    createLights() {
+        this.dLight = new THREE.DirectionalLight(0xffffff, .5);
+        this.dLight.position.set(0, 10, 0);
+        
+        this.aLight = new THREE.AmbientLight(0xffffff, .5);
+        
+//         for (let i = 0; i < this.spheres.length; i++) {
+//             const bLight = new THREE.PointLight(0x1111ff, 1, 200);
+//             bLight.position.set(10, 10, 20);
+//             bLight.target = this.spheres[i];
+//             this.scene.add(bLight);
+            
+//             const rLight = new THREE.PointLight(0xff11111, 1, 200);
+//             rLight.position.set(-10, 10, 20);
+//             rLight.target = this.spheres[i];
+//             this.scene.add(bLight);
+            
+//             this.lightSystems.push([bLight, rLight]);
+//         }
+        
+        // Point lights
+        this.pLightBlue = new THREE.PointLight(0x1111ff, 1, 200);
+        this.pLightBlue.position.set(10, 10, 20);
+        
+        this.pLightRed = new THREE.PointLight(0xff11111, 1, 200);
+        this.pLightRed.position.set(-10, 10, 20);
 
-    return shader;
-}
+        //this.scene.add(this.dLight);
+        this.scene.add(this.aLight);
+        this.scene.add(this.pLightBlue);
+        this.scene.add(this.pLightRed);
 
-function blit(target) {
-    if (target == null) {
-        gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-    } else {
-        gl.viewport(0, 0, target.width, target.height);
-        gl.bindFramebuffer(gl.FRAMEBUFFER, target.fbo);
     }
-    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
-}
+    
+    bindEvents() {
+        // window.addEventListener('mousemove', this.mousemove);
+        window.addEventListener('resize', this.resize);
+    }
+    
+    mousemove(e) {
+        this.mouse.x = -window.innerWidth + 2 * e.pageX;
+        this.mouse.y = -window.innerHeight + 2 * e.pageY;
+    }
+    
+    resize() {
+        const w = window.innerWidth;
+        const h = window.innerHeight;
+        console.log(w, h);
+        this.renderer.setSize(w,h);
+        this.camera.aspect = w/h;
+        this.camera.updateProjectionMatrix();
+    }
+    
+    moveSpheres() {
 
-function initFBOs() {
-    const fboSize = [
-        Math.floor(.5 * window.innerWidth),
-        Math.floor(.5 * window.innerHeight),
-    ]
-    outputColor = createDoubleFBO(fboSize[0], fboSize[1]);
-    velocity = createDoubleFBO(fboSize[0], fboSize[1], gl.RG);
-    divergence = createFBO(fboSize[0], fboSize[1], gl.RGB);
-    pressure = createDoubleFBO(fboSize[0], fboSize[1], gl.RGB);
-}
-
-
-function createFBO(w, h, type = gl.RGBA) {
-    gl.activeTexture(gl.TEXTURE0);
-
-    const texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texImage2D(gl.TEXTURE_2D, 0, type, w, h, 0, type, gl.FLOAT, null);
-
-    const fbo = gl.createFramebuffer();
-    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
-    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
-    gl.viewport(0, 0, w, h);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    return {
-        fbo,
-        width: w,
-        height: h,
-        attach(id) {
-            gl.activeTexture(gl.TEXTURE0 + id);
-            gl.bindTexture(gl.TEXTURE_2D, texture);
-            return id;
-        }
-    };
-}
-
-function createDoubleFBO(w, h, type) {
-    let fbo1 = createFBO(w, h, type);
-    let fbo2 = createFBO(w, h, type);
-
-    return {
-        width: w,
-        height: h,
-        texelSizeX: 1. / w,
-        texelSizeY: 1. / h,
-        read: () => {
-            return fbo1;
-        },
-        write: () => {
-            return fbo2;
-        },
-        swap() {
-            let temp = fbo1;
-            fbo1 = fbo2;
-            fbo2 = temp;
+        for (let i = 0; i < this.spheres.length; i++) {
+            const mesh = this.spheres[i].mesh;
+           mesh.material.uniforms.time.value = this.time;
+            const speed = .05 / this.spheres[i].mass;
+            const y = mesh.position.y + speed;
+            
+            mesh.position.y = y;
+            
+            if (y > 15) {
+                mesh.position.y = -20;
+                mesh.position.x = Math.random() * 20 - 10;
+            }
         }
     }
-}
-
-function render(t) {
-
-    const dt = 1 / 60;
-
-    if (t && isPreview) {
-        updateMousePosition(
-            (.5 - .45 * Math.sin(.003 * t - 2)) * window.innerWidth,
-            (.5 + .1 * Math.sin(.0025 * t) + .1 * Math.cos(.002 * t)) * window.innerHeight
-        );
+    
+    moveParticles() {
+        this.particles.material.uniforms.time.value = this.time;
+        // this.particles.material.needsUpdate = true;
     }
 
-    if (pointer.moved) {
-        if (!isPreview) {
-            pointer.moved = false;
-        }
-
-        gl.useProgram(splatProgram.program);
-        gl.uniform1i(splatProgram.uniforms.u_input_texture, velocity.read().attach(1));
-        gl.uniform1f(splatProgram.uniforms.u_ratio, canvasEl.width / canvasEl.height);
-        gl.uniform2f(splatProgram.uniforms.u_point, pointer.x / canvasEl.width, 1 - pointer.y / canvasEl.height);
-        gl.uniform3f(splatProgram.uniforms.u_point_value, pointer.dx, -pointer.dy, 1);
-        gl.uniform1f(splatProgram.uniforms.u_point_size, params.pointerSize);
-        blit(velocity.write());
-        velocity.swap();
-
-        gl.uniform1i(splatProgram.uniforms.u_input_texture, outputColor.read().attach(1));
-        gl.uniform3f(splatProgram.uniforms.u_point_value, 1. - params.color.r, 1. - params.color.g, 1. - params.color.b);
-        blit(outputColor.write());
-        outputColor.swap();
+    // Animations
+    
+    render() {
+        requestAnimationFrame(this.render);
+        this.time += .01;
+     
+        // this.moveCube();
+        this.moveSpheres();
+        this.moveParticles();
+        this.renderer.render(this.scene, this.camera);
     }
-
-    gl.useProgram(divergenceProgram.program);
-    gl.uniform2f(divergenceProgram.uniforms.u_texel, velocity.texelSizeX, velocity.texelSizeY);
-    gl.uniform1i(divergenceProgram.uniforms.u_velocity_texture, velocity.read().attach(1));
-    blit(divergence);
-
-    gl.useProgram(pressureProgram.program);
-    gl.uniform2f(pressureProgram.uniforms.u_texel, velocity.texelSizeX, velocity.texelSizeY);
-    gl.uniform1i(pressureProgram.uniforms.u_divergence_texture, divergence.attach(1));
-
-    for (let i = 0; i < 10; i++) {
-        gl.uniform1i(pressureProgram.uniforms.u_pressure_texture, pressure.read().attach(2));
-        blit(pressure.write());
-        pressure.swap();
+    
+    // Utils
+    degToRad(angle) {
+        return angle * Math.PI / 180;
     }
-
-    gl.useProgram(gradientSubtractProgram.program);
-    gl.uniform2f(gradientSubtractProgram.uniforms.u_texel, velocity.texelSizeX, velocity.texelSizeY);
-    gl.uniform1i(gradientSubtractProgram.uniforms.u_pressure_texture, pressure.read().attach(1));
-    gl.uniform1i(gradientSubtractProgram.uniforms.u_velocity_texture, velocity.read().attach(2));
-    blit(velocity.write());
-    velocity.swap();
-
-    gl.useProgram(advectionProgram.program);
-    gl.uniform1f(advectionProgram.uniforms.u_use_text, 0);
-    gl.uniform2f(advectionProgram.uniforms.u_texel, velocity.texelSizeX, velocity.texelSizeY);
-    gl.uniform1i(advectionProgram.uniforms.u_velocity_texture, velocity.read().attach(1));
-    gl.uniform1i(advectionProgram.uniforms.u_input_texture, velocity.read().attach(1));
-    gl.uniform1f(advectionProgram.uniforms.u_dt, dt);
-    blit(velocity.write());
-    velocity.swap();
-
-    gl.useProgram(advectionProgram.program);
-    gl.uniform1f(advectionProgram.uniforms.u_use_text, 1);
-    gl.uniform2f(advectionProgram.uniforms.u_texel, outputColor.texelSizeX, outputColor.texelSizeY);
-    gl.uniform1i(advectionProgram.uniforms.u_input_texture, outputColor.read().attach(2));
-    blit(outputColor.write());
-    outputColor.swap();
-
-    gl.useProgram(outputShaderProgram.program);
-    gl.uniform1i(outputShaderProgram.uniforms.u_output_texture, outputColor.read().attach(1));
-
-    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
-
-    requestAnimationFrame(render);
+    
 }
 
-function resizeCanvas() {
-    params.pointerSize = 4 / window.innerHeight;
-    canvasEl.width = textureEl.width = window.innerWidth;
-    canvasEl.height = textureEl.height = window.innerHeight;
-	 initFBOs();
-    updateTextCanvas();
-}
-
-function setupEvents() {
-    canvasEl.addEventListener("mousemove", (e) => {
-        isPreview = false;
-        updateMousePosition(e.pageX, e.pageY);
-    });
-
-    canvasEl.addEventListener("touchmove", (e) => {
-        e.preventDefault();
-        isPreview = false;
-        updateMousePosition(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
-    });
-}
-
-function updateMousePosition(eX, eY) {
-    pointer.moved = true;
-    pointer.dx = 5 * (eX - pointer.x);
-    pointer.dy = 5 * (eY - pointer.y);
-    pointer.x = eX;
-    pointer.y = eY;
-}
-
-function createControls() {
-    const gui = new GUI();
-	 gui.close();
-	
-    gui
-        .add(params, "text")
-        .onChange(updateTextCanvas);
-    gui
-        .add(params, "fontSize", 10, 300)
-        .onChange(updateTextCanvas)
-        .name("font size, px");
-    gui
-        .add(params, "isBold")
-        .onChange(updateTextCanvas)
-        .name("bold");
-    gui
-        .add(params, "fontName", Object.keys(fontOptions))
-        .onChange(updateTextCanvas)
-        .name("font");
-    gui
-        .addColor(params, "color");
-}
+const scene = new Scene({
+    el: document.querySelector('.container')
+});
